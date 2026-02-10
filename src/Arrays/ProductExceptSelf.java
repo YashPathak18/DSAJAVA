848466b3d09b0296c2321a9830e5d75748b3084d
YashPathak18 <yashpathak2106@gmail.com>
2026-02-10 23:56:01 +0530

    daily coding

---
+++ src/Arrays/ProductExceptSelf.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Prefix product
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Suffix product
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * suffix;
+            suffix = suffix * nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] ans = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
+
