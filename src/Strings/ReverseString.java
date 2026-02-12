35607ddcfbdd15f4be914a122ac8a2ae47390358
YashPathak18 <yashpathak2106@gmail.com>
2026-02-12 23:54:56 +0530

    daily coding

---
+++ src/Strings/ReverseString.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] arr = {'h','e','l','l','o'};
+        reverseString(arr);
+
+        System.out.println(arr);   // Output: olleh
+    }
+}
